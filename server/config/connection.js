9d9309da65e02e8dba6208791651256b6d1c9e73
Josh Stringer <joshstringer@live.com>
2024-09-28 15:48:41 -0500

    apollo server/graphql, mongodb now all working

---
+++ server/config/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://localhost:27017/excel_db';
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('MongodDB connected successfully');
+})
+.catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+
+module.exports = mongoose;
